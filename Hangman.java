cdee0b373b1998742971327baa0412d546207334
S <s>
2017-03-31 13:22:49 -0700

    {"types":"C","runs":16}

--- Hangman.java
+++ Hangman.java
@@ -50,6 +50,7 @@ public class Hangman extends ConsoleProgram {
     		println("Please enter guess letter");
     		guessChar = readLine();
     		if (guessChar.length() != 1){
+    			println("guessChar.length() = " + guessChar.length());
     			guessChar = "";
     		}
     		if (guessChar.charAt(0) > 'a' && guessChar.charAt(0) < 'z'){
