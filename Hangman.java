87faf47458025c542d1dfd98ebc2bd4cf443ef2d
S <s>
2017-03-31 12:14:05 -0700

    {"types":"C","runs":2}

--- Hangman.java
+++ Hangman.java
@@ -12,7 +12,14 @@ import acm.util.*;
 import java.awt.*;
 
 public class Hangman extends ConsoleProgram {
+	private HangmanLexicon hangmanLexicon = new HangmanLexicon();
 
+	//sets up the hangman game
+	public void init(){
+		println(hangmanLexicon);
+	}
+	
+	//executes playing hangman 
     public void run() {
 		/* You fill this in */
 	}
