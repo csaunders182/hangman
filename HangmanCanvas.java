fcf0fd6eaa287e454af8fa7065a76999607bf844
S <s>
2017-04-03 14:29:05 -0700

    {"types":"C","runs":44}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -182,7 +182,7 @@ public class HangmanCanvas extends GCanvas {
 			wordLabel.setLabel(word);
 			wordLabel.setLocation(getWidth()/2 - wordLabel.getWidth()/2, LABEL_Y_OFFSET);
 		} else {
-			guessLabel.setLabel(guessLabel + guessChar + " ");
+			guessLabel.setLabel("guessLabel + guessChar + ");
 			wordLabel.setLocation(getWidth()/2 - guessLabel.getWidth()/2, getHeight() - LABEL_Y_OFFSET/2);
 		}
 	}
